e43ccf874dcaec32751d5c9b444d0f3ea29d2232
Mavludin <imevlud@gmail.com>
2019-09-30 18:02:16 +0300

    Update script.js

--- quiz-app/script.js
+++ quiz-app/script.js
@@ -9,7 +9,11 @@ $(document).ready(function(){
 		heading.innerHTML = `Q${obj.id}. ${obj.question}`;
 		section.appendChild(heading);
 
+<<<<<<< HEAD
+			obj.options.map((item, pos) => {
+=======
 			obj.options.map((item,pos) => {
+>>>>>>> 4f4d84f2b13e59a769e552cf2966f3da8890e5d4
 				let div = document.createElement('div');
 				div.className = "option-wrapper";
 				let label = document.createElement('label');
@@ -42,6 +46,46 @@ $(document).ready(function(){
 			localAnswers = data.map(item => {
 				return item.answer;
 			});
+<<<<<<< HEAD
+			console.log(localAnswers);
+
+			$('.option-wrapper').click(function(e){
+				const input = $(this).find('input[type="radio"]');
+			
+				if ( input.prop('checked') ) {
+					var inputValue = parseInt(input.val());
+					var inputName = parseInt(input.attr('name').substr(1,1));
+
+					let flag = false;
+					for (let i=0; i<localAnswers.length; i++) {
+						if (localAnswers[i] == inputValue && i+1 == inputName) {
+							flag = true;
+							break;
+						} else {
+							flag = false;
+						}
+					}
+
+					if (flag) {
+						$(this).css('background-color', 'green');
+					} else {
+						$(this).css('background-color', 'red');
+						localAnswers.map((item, pos) => {
+							if (pos+1 == inputName) {
+								// console.log($(this).parent().find('.option-wrapper'));
+								console.log(input);
+								console.log(pos+1)
+							}
+						});
+					}
+
+
+				}
+
+			});
+
+=======
+>>>>>>> 4f4d84f2b13e59a769e552cf2966f3da8890e5d4
 			createSubmitButton();
 			processAnswers();
 		});
@@ -57,6 +101,24 @@ $(document).ready(function(){
 
 	const processAnswers = () => {
 
+<<<<<<< HEAD
+	    $('form').submit(function(e) {
+	    	e.preventDefault();
+		    $('#modal-wrapper').show();
+		    var result = $(this).serialize();
+    		result = result.substr(1, result.length).split("&");
+
+			let counter = 0;
+			for (let i=0;i<result.length;i++) {
+				if (result[i].substr(3,1) == localAnswers[i]) counter++;
+			}
+
+			$('#result-modal h3').html( $('#result-modal h3').html()+counter + "/5" );
+
+		});
+	
+	}
+=======
 			let asideCounter = 0;
 
 			$('.quiz-item:first input[type="radio"]').on('change', function () {
